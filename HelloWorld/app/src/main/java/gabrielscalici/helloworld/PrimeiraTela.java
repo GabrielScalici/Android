239c31478b6f5885619b8a31cb6c053d976284be
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-08-22 18:05:02 -0300

    botoes arrumados

--- HelloWorld/app/src/main/java/gabrielscalici/helloworld/PrimeiraTela.java
+++ HelloWorld/app/src/main/java/gabrielscalici/helloworld/PrimeiraTela.java
@@ -1,13 +1,57 @@
 package gabrielscalici.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class PrimeiraTela extends AppCompatActivity {
+
+    //Fazendo um botao para trocar de tela
+    Button botao_registrar;
+    CheckBox mostrarSenha;
+    EditText editTextSenha;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_cadastro);
+
+        //Reclama que os tipos sao diferentes do parametro, entao faz um cast
+        botao_registrar = (Button) findViewById(R.id.registrar_principal);
+        mostrarSenha = (CheckBox) findViewById(mostrarSenha);
+        editTextSenha = (EditText) findViewById(editTextSenha);
+
+
+        botao_registrar.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(PrimeiraTela.this, Segunda_Tela.class);
+                startActivity(i);
+            }
+
+        });
+
+
+        mostrarSenha.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+                if(mostrarSenha.isChecked()){
+                    editTextSenha.setTransformationMethod(null);
+                }else{
+                    editTextSenha.setTransformationMethod(new PasswordTransformationMethod());
+                }
+
+            }
+
+
+        });
+
     }
 }
---
+++ HelloWorld/app/src/main/java/gabrielscalici/helloworld/Segunda_Tela.java
@@ -0,0 +1,19 @@
+package gabrielscalici.helloworld;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gabrielscalici on 22/08/17.
+ */
+
+public class Segunda_Tela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_cadastro);
+
+
+    }
+}
