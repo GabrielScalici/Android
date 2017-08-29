5e8df1988f95ca7ce3f43109a78048af643c1077
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-08-29 18:25:12 -0300

    Usando ListView

--- Estudos_android/app/src/main/java/gabrielscalici/estudos_android/Main3Activity.java
+++ Estudos_android/app/src/main/java/gabrielscalici/estudos_android/Main3Activity.java
@@ -2,12 +2,23 @@ package gabrielscalici.estudos_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Main3Activity extends AppCompatActivity {
+
+
+    String[] paises = {"Argentina", "Brasil", "Chile", "Colombia", "Peru"}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        //manda para o id que fez no xml
+        //lista_paises = (ListView) findViewById(R.id.lista_paises);
+
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(Main3Activity.this)
+
     }
 }
