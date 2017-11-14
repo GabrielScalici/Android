5e104f75c2560a1313e03c1cbd90481dec8af6aa
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-11-14 18:51:32 -0200

    tempo criado

--- Forca_Jogo/app/src/main/java/gabrielscalici/forca_jogo/Tela_Venceu.java
+++ Forca_Jogo/app/src/main/java/gabrielscalici/forca_jogo/Tela_Venceu.java
@@ -1,13 +1,38 @@
 package gabrielscalici.forca_jogo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Tela_Venceu extends AppCompatActivity {
+
+    public Button btn_voltar;
+    public TextView txt_tempo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_venceu);
+
+        Intent intent = getIntent();
+       long tempo = intent.getLongExtra("tempo", -1);
+
+        btn_voltar = (Button) findViewById(R.id.btn_restart);
+        txt_tempo = (TextView) findViewById(R.id.txt_timer);
+
+        txt_tempo.setText(String.valueOf(tempo/1000 + " segundos"));
+
+        btn_voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Tela_Venceu.this, Main.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- Forca_Jogo/app/src/main/java/gabrielscalici/forca_jogo/tela_forca_jogo.java
+++ Forca_Jogo/app/src/main/java/gabrielscalici/forca_jogo/tela_forca_jogo.java
@@ -11,13 +11,17 @@ import android.widget.TextView;
 public class tela_forca_jogo extends AppCompatActivity {
 
     //Armazenar a quantidade de erros
-    int quantidade_vidas = 6;
+    int quantidade_vidas = 7;
     String palavra = "FELIPE";
+    String segredo;
     int getQuantidade_erros;
-
+    long startTime;
 
     public TextView exibir_vidas;
+    public TextView txt_segredo;
     public Button btn_submeter;
+    public EditText caixa;
+
 
 
 
@@ -26,24 +30,39 @@ public class tela_forca_jogo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_forca_jogo);
 
+        startTime = System.currentTimeMillis();
+
+
         exibir_vidas = (TextView) findViewById(R.id.txt_vidas);
         exibir_vidas.setText(String.valueOf(quantidade_vidas));
 
-        btn_submeter = (Button) findViewById(R.id.btn_submeter);
 
+        caixa = (EditText) findViewById(R.id.edt_palavra);
+
+        btn_submeter = (Button) findViewById(R.id.btn_submeter);
         btn_submeter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                quantidade_vidas -= 1;
-                exibir_vidas.setText(String.valueOf(quantidade_vidas));
-
-                if(quantidade_vidas <= 0){
-
-                    tela_perdeu(v);
+                //quantidade_vidas -= 1;
 
 
+                //Pegnado o texto
+                String txt = caixa.getText().toString();
+
+                if(txt.compareTo(palavra)==0){
+                    long estimatedTime = System.currentTimeMillis() - startTime;
+                    tela_venceu(v, estimatedTime);
+
+                }else{
+                    quantidade_vidas -= 1;
                 }
 
+
+                if(quantidade_vidas <= 0){
+                    tela_perdeu(v);
+                }
+
+                exibir_vidas.setText(String.valueOf(quantidade_vidas));
             }
         });
 
@@ -57,5 +76,14 @@ public class tela_forca_jogo extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void tela_venceu(View view, long time){
+        Intent intent = new Intent(tela_forca_jogo.this, Tela_Venceu.class);
+        intent.putExtra("tempo", time);
+        startActivity(intent);
+    }
+
+    public void exibir_palavra(){
+
+    }
 
 }
