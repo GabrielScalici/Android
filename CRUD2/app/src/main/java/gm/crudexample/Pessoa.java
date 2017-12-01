cb9fd7b912c4751be9ce7c167f5765668df265be
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-12-01 21:36:22 -0200

    Projeto de exemplo

---
+++ CRUD2/app/src/main/java/gm/crudexample/Pessoa.java
@@ -0,0 +1,63 @@
+package gm.crudexample;
+
+import java.util.Random;
+
+/**
+ * Criado por Gilian Marques em 12/12/2016.
+ */
+public class Pessoa {
+
+
+    public Pessoa(String UID) {
+        this.UID = UID;
+    }
+
+    private final String UID;
+    private String nome;
+    private int idade;
+    private double peso;
+    private boolean deficiencia;
+
+    public String getUID() {
+        return UID;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public boolean isDeficiente() {
+        return deficiencia;
+    }
+
+    public void setDeficiencia(boolean deficiencia) {
+        this.deficiencia = deficiencia;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    /**
+     * @return Uma identificaçao unica para o objeto Pessoa.
+     */
+    public static String criarUID() {
+        return "@Pessoa:" + System.currentTimeMillis() + new Random().nextDouble();
+    }
+}
